17191492815552535a25fa644cd4eb446f49ec08
walace <walacefex01@gmail.com>
2021-04-05 23:40:47 -0300

    finalizando o boilerplate com os testes e styled components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,9 +5,17 @@ import Main from '.'
 describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
+
     expect(
-      screen.getByRole('heading', { name: /React Avançado/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
